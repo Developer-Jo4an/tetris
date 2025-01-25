69c8723eb4560c5d08d4a7c188e074b8755fae65
Developer-Jo4an <107997257+Developer-Jo4an@users.noreply.github.com>
2025-01-25 16:09:10 +0300

    chore(globalUtils): добавлен знак

--- controllers/tetris/utils/globalUtils.js
+++ controllers/tetris/utils/globalUtils.js
@@ -3,6 +3,6 @@ export const globalUtils = {
     const marginFromEdge = gameSize.width * margin;
     const maxStringLength = grid.reduce((acc, {cells}) => Math.max(cells.length, acc), 0);
     const maxValue = Math.max(maxStringLength, grid.length);
-    return (gameSize.width - 2 * marginFromEdge) / maxStringLength/*maxValue*/;
+    return (gameSize.width - 2 * marginFromEdge) / maxStringLength /*- maxValue*/;
   }
 };
